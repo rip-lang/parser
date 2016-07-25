2ddbc2f7eeae6dcc360e5c61eaaf0c7b04ab009e
Thomas Ingram <code@ravinggenius.com>
2016-07-25 19:19:09 -0400

    WIP Normalize parse tree nodes

--- source/rip/parser/utilities/normalizer.rb
+++ source/rip/parser/utilities/normalizer.rb
@@ -1,3 +1,4 @@
+require 'hashie'
 require 'parslet'
 
 module Rip::Parser::Utilities
@@ -8,6 +9,136 @@ module Rip::Parser::Utilities
 
     def self.apply(origin, raw_tree)
       new.apply(raw_tree, origin: origin)
+    end
+
+
+    rule(import: simple(:location), module_name: simple(:module_name)) do |location:, module_name:, origin:|
+      Hashie::Mash.new(
+        module_name: module_name,
+        location: Rip::Parser::Location.from_slice(origin, location, location.length + module_name.location.offset - location.offset)
+      )
+    end
+
+
+    rule(integer: simple(:integer)) do |integer:, origin:|
+      Hashie::Mash.new(
+        sign: :+,
+        integer: integer.to_s,
+        location: Rip::Parser::Location.from_slice(origin, integer, integer.length)
+      )
+    end
+
+    rule(integer: simple(:integer), decimal: simple(:decimal)) do |integer:, decimal:, origin:|
+      Hashie::Mash.new(
+        sign: :+,
+        integer: integer.to_s,
+        decimal: decimal.to_s,
+        location: Rip::Parser::Location.from_slice(origin, integer, integer.length + decimal.length)
+      )
+    end
+
+    rule(sign: simple(:sign), integer: simple(:integer)) do |sign:, integer:, origin:|
+      Hashie::Mash.new(
+        sign: sign.to_sym,
+        integer: integer.to_s,
+        location: Rip::Parser::Location.from_slice(origin, sign, sign.length + integer.length)
+      )
+    end
+
+    rule(sign: simple(:sign), integer: simple(:integer), decimal: simple(:decimal)) do |sign:, integer:, decimal:, origin:|
+      Hashie::Mash.new(
+        sign: sign.to_sym,
+        integer: integer.to_s,
+        decimal: decimal.to_s,
+        location: Rip::Parser::Location.from_slice(origin, sign, sign.length + integer.length + decimal.length)
+      )
+    end
+
+
+    rule(escape_unicode: simple(:sequence), escape_location: simple(:location)) do |sequence:, location:, origin:|
+      Hashie::Mash.new(
+        escape_unicode: sequence.to_s,
+        location: Rip::Parser::Location.from_slice(origin, location, location.length + sequence.length)
+      )
+    end
+
+    rule(escape_special: simple(:sequence), escape_location: simple(:location)) do |sequence:, location:, origin:|
+      Hashie::Mash.new(
+        escape_special: sequence.to_s,
+        location: Rip::Parser::Location.from_slice(origin, location, location.length + sequence.length)
+      )
+    end
+
+    rule(escape_any: simple(:sequence), escape_location: simple(:location)) do |sequence:, location:, origin:|
+      Hashie::Mash.new(
+        escape_any: sequence.to_s,
+        location: Rip::Parser::Location.from_slice(origin, location, location.length + sequence.length)
+      )
+    end
+
+
+    rule(character: simple(:character)) do |character:, origin:|
+      Hashie::Mash.new(
+        character: character.to_s,
+        location: Rip::Parser::Location.from_slice(origin, character, character.length)
+      )
+    end
+
+    rule(character: simple(:character), location: simple(:location)) do |character:, location:, origin:|
+      Hashie::Mash.new(
+        character: character.to_s,
+        location: Rip::Parser::Location.from_slice(origin, location, location.length + character.length)
+      )
+    end
+
+
+    rule(regular_expression: sequence(:characters), location: simple(:location)) do |characters:, location:, origin:|
+      length = characters.inject(location.length + 1) do |memo, character|
+        memo + character.location.length
+      end
+
+      Hashie::Mash.new(
+        regular_expression: characters,
+        location: Rip::Parser::Location.from_slice(origin, location, length)
+      )
+    end
+
+
+    rule(string: sequence(:characters), location: simple(:location)) do |characters:, location:, origin:|
+      closing_delimiter_length = case location
+        when /:/                       then 0
+        when /"/                       then 1
+        when /\A\<\<(?<label>[A-Z_]+)/ then Regexp.last_match(:label).length
+      end
+
+      length = characters.inject(location.length + closing_delimiter_length) do |memo, character|
+        memo + character.location.length
+      end
+
+      Hashie::Mash.new(
+        string: characters,
+        location: Rip::Parser::Location.from_slice(origin, location, length)
+      )
+    end
+
+
+    rule(list: sequence(:items), location: simple(:location)) do |items:, location:, origin:|
+      length = items.inject(location.length) do |memo, item|
+        memo + item.location.length
+      end
+
+      Hashie::Mash.new(
+        list: items,
+        location: Rip::Parser::Location.from_slice(origin, location, length)
+      )
+    end
+
+
+    rule(reference: simple(:reference)) do |reference:, origin:|
+      Hashie::Mash.new(
+        reference: reference.to_s,
+        location: Rip::Parser::Location.from_slice(origin, reference)
+      )
     end
   end
 end
