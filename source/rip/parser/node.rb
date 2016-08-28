729f42a38aa1c00e122aa6b4b01c50d79d08af69
Thomas Ingram <code@ravinggenius.com>
2016-08-28 17:38:07 -0400

    Merge hash representation methods for AST node as they were virtually the same

--- source/rip/parser/node.rb
+++ source/rip/parser/node.rb
@@ -46,20 +46,16 @@ module Rip::Parser
       self.class.new(extra.merge(other.to_h).merge(location: location, type: other[:type] || type))
     end
 
-    def s_expression
+    def to_h(include_location: true)
       _extra = extra.map do |key, value|
-        [ key, self.class.try_convert_s_expression(value) ]
+        [ key, self.class.try_convert_to_h(value, include_location) ]
       end.to_h
 
-      { type: type }.merge(_extra)
-    end
-
-    def to_h
-      _extra = extra.map do |key, value|
-        [ key, self.class.try_convert_to_h(value) ]
-      end.to_h
-
-      { location: location, type: type }.merge(_extra)
+      if include_location
+        { location: location, type: type }
+      else
+        { type: type }
+      end.merge(_extra)
     end
 
     def traverse(&callback)
@@ -109,23 +105,14 @@ module Rip::Parser
       end
     end
 
-    def self.try_convert_to_h(value)
+    def self.try_convert_to_h(value, include_location)
       case value
       when Array
-        value.map(&method(:try_convert_to_h))
+        value.map do |v|
+          try_convert_to_h(v, include_location)
+        end
       when self
-        value.to_h
-      else
-        value
-      end
-    end
-
-    def self.try_convert_s_expression(value)
-      case value
-      when Array
-        value.map(&method(:try_convert_s_expression))
-      when self
-        value.s_expression
+        value.to_h(include_location: include_location)
       else
         value
       end
